0c897346cf87ecab71ebbcd5d354614b2f98594e
Vadym Bezkyshkyi <vadym.bezkyshkyi@gmail.com>
2020-12-05 19:08:49 +0200

    Videos listening messages

--- Hackathon-VideoSharing-Platform/Server/Hubs/NotifucationHub.cs
+++ Hackathon-VideoSharing-Platform/Server/Hubs/NotifucationHub.cs
@@ -1,14 +1,16 @@
 ﻿using Hackathon_VideoSharing_Platform.Shared;
 using Microsoft.AspNetCore.SignalR;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace Hackathon_VideoSharing_Platform.Server.Hubs
 {
     public class NotifucationHub : Hub
     {
-        async Task BroadcastNewVideoInfo(VideoMetaData videoMetaData)
+        public async Task BroadcastNewVideoInfo(VideoMetaData videoMetaData)
         {
             await Clients.All.SendAsync("OnBroadcastNewVideoInfo", videoMetaData);
+            Debug.WriteLine("Send video");
         }
     }
 }
