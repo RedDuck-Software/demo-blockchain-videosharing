6b3707acd14fb2b3a9037b5478b70a70b13994a8
Mark Virchenko <mark.virchenko@calienteam.com>
2020-12-06 12:00:47 +0200

    add test for nethereum

---
+++ IPFSConnection/NethereumContract.cs
@@ -0,0 +1,23 @@
+﻿using Nethereum.Web3;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLib
+{
+    public class NethereumContract
+    {
+        private readonly Web3 web3;
+
+        public NethereumContract(Web3 web3)
+        {
+            this.web3 = web3;
+        }
+
+        public async Task BuyVideoRequestAsync(string sellerAddress, string publicKey, string videoName, decimal etherAmount)
+        {
+
+        }
+    }
+}
--- NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/UnitTest1.cs
@@ -1,3 +1,4 @@
+using Nethereum.Web3;
 using NUnit.Framework;
 using System.Threading.Tasks;
 
@@ -18,6 +19,16 @@ namespace NUnitTestProject1
             var str = await con.UploadDataAsync(System.Text.Encoding.UTF8.GetBytes("tratata"));
 
             Assert.IsNotNull(str);
+        }
+
+        [Test]
+        public async Task Test2()
+        {
+            var web3 = new Web3("");
+
+            var lib = new CommonLib.NethereumContract(web3);
+
+            await lib.BuyVideoRequestAsync("", "", "", 0.005m);
         }
     }
 }
