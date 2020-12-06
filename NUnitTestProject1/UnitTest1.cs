9456fe28bb32696f7c5644253719865aec29645f
Roman Yavnikov <romazes20@gmail.com>
2020-12-06 16:33:30 +0200

    Add GetVideo and Unit and display test Video.

--- NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/UnitTest1.cs
@@ -1,5 +1,6 @@
 using Nethereum.Web3;
 using NUnit.Framework;
+using System;
 using System.Threading.Tasks;
 
 namespace NUnitTestProject1
@@ -29,6 +30,20 @@ namespace NUnitTestProject1
             var lib = new CommonLib.NethereumContract(web3);
 
             await lib.BuyVideoRequestAsync("", "", "", 0.005m);
+        }
+
+        [Test]
+        public async Task StreamVideo()
+        {
+            var connect = new IPFSConnection.IPFSConnection();
+
+            var expected = System.Text.Encoding.UTF8.GetBytes("tratata");
+
+            var fileHash = await connect.UploadDataAsync(expected);
+
+            var actual = await connect.GetVideoData64(fileHash);
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
